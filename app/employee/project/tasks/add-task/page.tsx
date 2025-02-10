f08e091bdfa2c48a65af819a3354112f08f74425
Jiro Bañaga <jirobanaga@gmail.com>
2025-02-10 17:04:16 +0800

    Routing

---
+++ app/employee/project/tasks/add-task/page.tsx
@@ -0,0 +1,23 @@
+import Breadcrumbs from '@/app/ui/breadcrumbs';
+import AddTask from '@/app/ui/employee/project/tasks/add-task-form';
+
+export default async function Page() {
+  return (
+    <main className='w-full h-auto flex justify-center flex-col'>
+      <Breadcrumbs
+        breadcrumbs={[
+          {
+            label: 'Tasks',
+            href: '/employee/project/tasks',
+          },
+          {
+            label: 'Add Task',
+            href: '/employee/project/tasks/add-task',
+            active: true,
+          },
+        ]}
+      />
+      <AddTask />
+    </main>
+  );
+}
