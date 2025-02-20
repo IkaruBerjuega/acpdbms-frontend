7883947e4ff8374ad7e68076782e7d062aa95fc5
Ikaru Berjuega <francisjamespberjuega@gmail.com>
2025-02-21 02:38:37 +0800

    homepage without recent projects, contact form and login integration. Created hooks for api calls

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,15 @@
-import { Combobox } from '@/components/ui/combobox';
-import Image from 'next/image';
+import About from "@/components/ui/homepage/about";
+import Hero from "@/components/ui/homepage/hero";
+import Navbar from "@/components/ui/homepage/navbar";
+import Process from "@/components/ui/homepage/process";
 
 export default function Home() {
-  const items = [{ label: 'meow', value: 'meow' }];
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <Combobox
-        items={[
-          { label: 'Option 1', value: '1' },
-          { label: 'Option 2', value: '2' },
-        ]}
-        placeholder='Select an option'
-        emptyMessage='No items found'
-      />
-    </div>
+    <main className="">
+      <Navbar />
+      <Hero />
+      <About />
+      <Process />
+    </main>
   );
 }
