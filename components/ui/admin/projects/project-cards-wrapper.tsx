eb07a6c6ef8c26d38af68826c1f3932f7bd9b1f0
Ikaru Berjuega <francisjamespberjuega@gmail.com>
2025-04-21 18:02:52 +0800

    fix final

--- components/ui/admin/projects/project-cards-wrapper.tsx
+++ components/ui/admin/projects/project-cards-wrapper.tsx
@@ -4,19 +4,21 @@ import ProjectCards from "../../general/data-table-components/project-cards";
 import { columns } from "./project-columns";
 import { ProjectListResponseInterface } from "@/lib/definitions";
 
-export default function Cards<T extends ProjectListResponseInterface>({
+export default function Cards({
   isArchived,
   initialData,
 }: {
   isArchived: boolean;
-  initialData: T[];
+  initialData: ProjectListResponseInterface[];
 }) {
-  const transformedColumns = useCreateTableColumns<T>(columns, "Projects");
+  const transformedColumns =
+    useCreateTableColumns<ProjectListResponseInterface>(columns, "Projects");
 
-  const { data: projectList, isLoading } = useProjectList<T>({
-    isArchived: isArchived,
-    initialData: initialData,
-  });
+  const { data: projectList, isLoading } =
+    useProjectList<ProjectListResponseInterface>({
+      isArchived: isArchived,
+      initialData: initialData,
+    });
 
   if (isLoading) return <p>Loading...</p>;
   if (!projectList) return <>No Projects Yet</>;
