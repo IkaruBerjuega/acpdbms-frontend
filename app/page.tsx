4c7017931896ac50ff60ec473a1655eefe40961a
Ikaru Berjuega <francisjamespberjuega@gmail.com>
2025-05-01 20:21:21 +0800

    revert homepage to ssr, used loading.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import About from "@/components/ui/homepage/about";
 import ContactForm from "@/components/ui/homepage/contact-form";
 import Footer from "@/components/ui/homepage/footer";
@@ -7,20 +5,39 @@ import Hero from "@/components/ui/homepage/hero";
 import Navbar from "@/components/ui/homepage/navbar";
 import Process from "@/components/ui/homepage/process";
 import RecentProjects from "@/components/ui/homepage/recent-projects";
-import { useAdminSettings } from "@/hooks/general/use-admin-settings";
+import serverRequestAPI from "@/hooks/server-request";
+import {
+  DynamicContactSchema,
+  LogoResponse,
+  RecentProjectsResponse,
+} from "@/lib/definitions";
 
-export default function Page() {
-  const { logoQuery, contactDetailsQuery, recentImagesQuery } =
-    useAdminSettings();
+export default async function Home() {
+  const logoResponse: LogoResponse | null =
+    (await serverRequestAPI({
+      url: "/settings/logo",
+      auth: false,
+    })) || "/system-component-images/logo-placeholder.webp";
+
+  const contactDetailsResponse: DynamicContactSchema | null =
+    await serverRequestAPI({
+      url: "/contact-details",
+      auth: false,
+    });
+
+  const recentProjectImagesResponse: RecentProjectsResponse | null =
+    await serverRequestAPI({
+      url: "/recent-projects",
+      auth: false,
+    });
 
   const logoUrl =
-    logoQuery?.data?.logo_url ||
-    "/system-component-images/logo-placeholder.webp";
+    logoResponse?.logo_url || "/system-component-images/logo-placeholder.webp";
 
-  const contactDetails = contactDetailsQuery?.data?.contact_details || [];
+  const contactDetails = contactDetailsResponse?.contact_details || [];
 
   const recentProjectImages =
-    recentImagesQuery?.data?.recent_project_images || [];
+    recentProjectImagesResponse?.recent_project_images || [];
 
   return (
     <main className="relative">
